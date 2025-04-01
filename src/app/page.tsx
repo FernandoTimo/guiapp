8792cf6e0ec09840500ea58fff08b6af71e65d4c
Timoideas <Timoideas@hotmail.com>
2025-04-01 09:58:59 -0500

    HABER X5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Editor from "@/components/editor/Editor";
 import Sidebar from "@/components/sidebar/SideBar";
-
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+// Carga dinámica de Editor como componente cliente
+const Editor = dynamic(() => import("@/components/editor/Editor"), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <Editor />
+      <Suspense fallback={<div>Cargando editor...</div>}>
+        <Editor />
+      </Suspense>
     </div>
   );
 }
