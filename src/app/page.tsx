c7cdb709f57b69442dfaff1c8c96c9f8a2c29b75
Timoideas <Timoideas@hotmail.com>
2025-04-01 10:06:24 -0500

    HABER X6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Sidebar from "@/components/sidebar/SideBar";
 import dynamic from "next/dynamic";
+import Sidebar from "@/components/sidebar/SideBar";
 import { Suspense } from "react";
-// Carga dinámica de Editor como componente cliente
+
+// ⬇️ Cargar Editor dinámicamente como client component
 const Editor = dynamic(() => import("@/components/editor/Editor"), {
   ssr: false,
 });
+
 export default function Home() {
   return (
     <div className="flex min-h-screen">
